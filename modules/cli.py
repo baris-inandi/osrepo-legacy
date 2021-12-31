fda74b96a86e8828d8bb1dc8d53466ba13f36069
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-12-31 07:35:47 +0300

    wip repo object

--- modules/cli.py
+++ modules/cli.py
@@ -1,15 +1,11 @@
 from modules import select
 from modules.search import search
 from modules.tools import abort
-from modules.repo import repo
-from modules.entry import Entry
-from modules.fmt import Fmt
-from modules.tools import confirm
+from modules.repo import repo_object
 
 
 def search_os(keyword: str):
     # osr list arch
-
     # returns the download URL of selected os/version.
     search_result, elapsed = search(keyword)
     if len(search_result) > 0:
@@ -23,15 +19,7 @@ def search_os(keyword: str):
 
 def list_all():
     # osr list *
-
-    # get confirmation:
-    # [?] This will print out all the available OS entries. Are you sure?
-    if confirm(msg="This will print out ALL OS entries. Are you sure?",
-               default=False):
-        for index, name in enumerate(repo.keys()):
-            print(Fmt.color("header", str(index + 1)), Entry(name))
-    else:
-        abort("Aborted by user.")
+    return repo_object.list_all()
 
 
 def download_with_id():
--- modules/repo.py
+++ modules/repo.py
@@ -1,33 +1,36 @@
 from yaml import safe_load as load_yaml
-
-
-def load_repo():
-    # load core repo
-    with open("osrepo.yaml", "r") as stream:
-        core_repo = load_yaml(stream)
-    for key in core_repo["osr"]:
-        core_repo["osr"][key]["repo"] = "core"
-
-    # load community repo
-    with open("community.yaml", "r") as stream:
-        community_repo = load_yaml(stream)
-    for key in community_repo["osr"]:
-        community_repo["osr"][key]["repo"] = "community"
-
-    return {**community_repo["osr"], **core_repo["osr"]}
-
-
-repo = load_repo()
+from modules.tools import abort
+from modules.entry import Entry
+from modules.fmt import Fmt
+from modules.tools import confirm
 
 
 class Repo:
     def __init__(self, repo_files: list[tuple]):
         # example repo_files format:
         # [("osrepo.yaml", "core"), ("community.yaml", "community")]
-        pass
+        sub_repo_list, repo = [], {}
+        for file in repo_files:
+            sub_repo_list.append(self.parse(*file))
+        for sub_repo in sub_repo_list:
+            repo.update(sub_repo)
+        self.repo = repo
 
     def parse(self, filename: str, repo_name: str):
-        with open("osrepo.yaml", "r") as stream:
-            core_repo = load_yaml(stream)
-        for key in core_repo["osr"]:
-            core_repo["osr"][key]["repo"] = "core"
+        with open(filename, "r") as stream:
+            loaded = load_yaml(stream)
+        for key in loaded["osr"]:
+            loaded["osr"][key]["repo"] = repo_name
+        return loaded["osr"]
+
+    def list_all(self):
+        # TODO: abort not working
+        if confirm(msg="This will print out *ALL* OS entries. Are you sure?"):
+            for index, name in enumerate(self.repo.keys()):
+                print(Fmt.color("header", str(index + 1)), Entry(name), "\n")
+        else:
+            abort("Aborted by user.")
+
+
+repo_object = Repo([("community.yaml", "community"), ("osrepo.yaml", "core")])
+repo = repo_object.repo
